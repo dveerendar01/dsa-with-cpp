c01f66bf93fc59b36ca70ab27fad0fee338b25bf
dveerendar01 <veerendarrathod4@gmail.com>
2026-08-20 20:46:16 +0530

    Add rotate array left by d times

--- 06_Array.cpp/11_rotate_array_left_d-times_2.cpp
+++ 06_Array.cpp/11_rotate_array_left_d-times_2.cpp
@@ -2,40 +2,34 @@
 using namespace std;
 
 void leftRotate(int arr[], int n, int d) {
-    d = d % n; // To handle cases where d > n
+    d = d % n;
 
-    int temp[d];
-    for(int i=0; i<d; i++) {
-        temp[i] = arr[i];
-    }
-
-    for(int i=d; i<n; i++) {
-        arr[i-d] = arr[i];
-    }
-
-    for(int i=n-d; i<n; i++) {
-        arr[i] = temp[i-(n-d)];
-    }
+    reverse(arr, arr+d);
+    reverse(arr+d, arr+n);
+    reverse(arr, arr+n);
 }
 
 int main() {
     int n;
-    cout << "Enter the array size: ";
+    cout << "Enter array size: ";
     cin >> n;
+
     int arr[n];
-    cout << "Enter the array elements: ";
+    cout << "Enter array elements: ";
     for(int i=0; i<n; i++) {
         cin >> arr[i];
     }
+
     int d;
     cout << "Enter number of positions to rotate: ";
     cin >> d;
     leftRotate(arr, n, d);
+    cout << "Array after left roatation: ";
     for(int i=0; i<n; i++) {
         cout << arr[i] << " ";
     }
     return 0;
 }
 
-// Time Complexity: O(n+d)
-// Space Complexity: O(d)
+// Time Complexity: O(2n)
+// Space Complexity: O(1)
