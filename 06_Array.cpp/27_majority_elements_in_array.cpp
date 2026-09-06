9fae8510a8dfa523d17dd1850b21503ee9f12fdb
dveerendar01 <veerendarrathod4@gmail.com>
2026-09-07 00:46:52 +0530

    Add Majority Element in an Array

--- 06_Array.cpp/27_majority_elements_in_array.cpp
+++ 06_Array.cpp/27_majority_elements_in_array.cpp
@@ -1,43 +1,49 @@
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-int majorityElement(vector<int>& nums) {
-    int candidate = 0;
+int majorityElement(vector<int>& arr) {
     int cnt = 0;
-
-    for(int num : nums) {
-
+    int element = 0;
+    for(int i=0; i<arr.size(); i++) {
         if(cnt == 0) {
-            candidate = num;
-        }
-
-        if(num == candidate) {
+            cnt = 1;
+            element = arr[i];
+        } else if(arr[i] == element) {
             cnt++;
         } else {
             cnt--;
         }
     }
-
-    return candidate;
+    int cnt1 = 0;
+    for(int i=0; i<arr.size(); i++) {
+        if(arr[i] == element) {
+            cnt1++;
+        }
+    }
+    if(cnt1 > (arr.size() / 2)) {
+        return element;
+    }
+    return -1;
 }
 
 int main() {
-
     int n;
-
-    cout << "Enter the number of elements: ";
+    cout << "Enter array size: ";
     cin >> n;
-
-    vector<int> nums(n);
-
-    cout << "Enter the elements: ";
-    for(int i = 0; i < n; i++) {
-        cin >> nums[i];
+    if(n <= 0) {
+        cout << "Invalid array size" << endl;
+        return 0;
     }
-
-    int answer = majorityElement(nums);
-
-    cout << "Majority Element: " << answer << endl;
-
+    vector<int> arr(n);
+    cout << "Enter array elements: ";
+    for(int i=0; i<n; i++) {
+        cin >> arr[i];
+    }
+    int answ = majorityElement(arr);
+    if(answ == -1) {
+        cout << "No majority element" << endl;
+    } else {
+        cout << "Majority element is: " << answ << "\n";
+    }
     return 0;
 }
