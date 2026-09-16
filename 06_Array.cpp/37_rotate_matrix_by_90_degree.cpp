1f2f7a95178ea6c2c5ebd0fa5135fa1633fcdd6d
dveerendar01 <veerendarrathod4@gmail.com>
2026-09-16 21:08:25 +0530

    Add Rotate Matrix by 90 degree Solution

---
+++ 06_Array.cpp/37_rotate_matrix_by_90_degree.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void rotateMatrix(vector<vector<int>>& mat) {
+    int n = mat.size();
+    for(int i=0; i<n-1; i++) {
+        for(int j=i+1; j<n; j++) {
+            swap(mat[i][j], mat[j][i]);
+        }
+    }
+    for(int i=0; i<n; i++) {
+        reverse(mat[i].begin(), mat[i].end());
+    }
+}
+
+int main() {
+    int n;
+    cout << "Enter number of rows:\n";
+    cin >> n;
+
+    vector<vector<int>> mat(n, vector<int>(n));
+    cout << "Enter Matrix Elements:\n";
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    rotateMatrix(mat);
+    cout << "Matrix after the rotation is:\n";
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cout << mat[i][j] << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
