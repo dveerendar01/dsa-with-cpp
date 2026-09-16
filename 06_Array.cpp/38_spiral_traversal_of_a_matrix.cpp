b6861d4ba5cd0421e0bc368d3e0c2eee96ed7f1e
dveerendar01 <veerendarrathod4@gmail.com>
2026-09-16 23:54:49 +0530

    Add Spiral Traversal of a Matrix Solution

---
+++ 06_Array.cpp/38_spiral_traversal_of_a_matrix.cpp
@@ -0,0 +1,70 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> spiralTraversal(vector<vector<int>>& mat) {
+    int n = mat.size();
+    int m = mat[0].size();
+
+    int top = 0;
+    int bottom = n-1;
+    int left = 0;
+    int right = m-1;
+
+    vector<int> ans;
+
+    while(top <= bottom && left <= right) {
+        // Left to Right
+        for(int j=left; j<=right; j++) {
+            ans.push_back(mat[top][j]);
+        }
+        top++;
+
+        // Top to Bottom
+        for(int i=top; i<=bottom; i++) {
+            ans.push_back(mat[i][right]);
+        }
+        right--;
+
+        // Right to Left
+        if(top <= bottom) {
+            for(int j=right; j>=left; j--) {
+                ans.push_back(mat[bottom][j]);
+            }
+            bottom--;
+        }
+
+        // Bottom to Top
+        if(left <= right) {
+            for(int i=bottom; i>=top; i--) {
+                ans.push_back(mat[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return ans;
+}
+
+int main() {
+    int n,m;
+    cout << "Enter number of rows: ";
+    cin >> n;
+    cout << "Enter number of cols: ";
+    cin >> m;
+
+    vector<vector<int>> mat(n,vector<int>(m));
+    cout << "Enter Matrix Elements:\n";
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<m; j++) {
+            cin >> mat[i][j];
+        }
+    }
+
+    vector<int> ans = spiralTraversal(mat);
+    cout << "Spiral Traversal:\n";
+    for(int num : ans) {
+        cout << num << " ";
+    }
+    cout << endl;
+    return 0;
+}
