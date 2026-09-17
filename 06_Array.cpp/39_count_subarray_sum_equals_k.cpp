c87f6bae57af53cd6e1bf893d14f7b8dbfd8a4cd
dveerendar01 <veerendarrathod4@gmail.com>
2026-09-17 23:58:45 +0530

    Add Subarray Sum Equals K Solution

---
+++ 06_Array.cpp/39_count_subarray_sum_equals_k.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int subarraySum(vector<int>& nums,int k) {
+    unordered_map<int,int> prefixSumMap;
+    prefixSumMap[0] = 1;
+    int sum = 0;
+    int count = 0;
+
+    for(int i=0; i<nums.size(); i++) {
+        sum += nums[i];
+        int rem = sum-k;
+        if(prefixSumMap.find(rem) != prefixSumMap.end()) {
+            count += prefixSumMap[rem];
+        }
+        prefixSumMap[sum]++;
+    }
+    return count;
+}
+
+int main() {
+    int n,k;
+    cout << "Enter array size: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter array elements: ";
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    cout << "Enter K: ";
+    cin >> k;
+
+    int ans = subarraySum(nums,k);
+    cout << "Number of subarrays with sum K: " << ans << endl;
+    return 0;
+}
