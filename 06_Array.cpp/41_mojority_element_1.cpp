535de6d62eabd0291c575a7c83bc1622158f0e06
dveerendar01 <veerendarrathod4@gmail.com>
2026-09-21 20:33:29 +0530

    Add Majority Elements Solution

---
+++ 06_Array.cpp/41_mojority_element_1.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+vector<int> majorityElement(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> ls; // list
+    map<int, int> mpp;
+    int min = (int)(n/3) + 1;
+    for(int i=0; i<n; i++) {
+        mpp[nums[i]]++;
+        if(mpp[nums[i]] == min) {
+            ls.push_back(nums[i]);
+        }
+        if(ls.size() == 2) break;
+    }
+    sort(ls.begin(), ls.end());
+    return ls;
+}
+
+int main() {
+    int n;
+    cout << "Enter size: ";
+    cin >> n;
+
+    if(n <= 0) {
+        cout << "Size is Invalid";
+        return 0;
+    }
+
+    vector<int> nums(n);
+    cout << "Enter the elements: ";
+    for(int i=0; i<n; i++) {
+        cin >> nums[i];
+    }
+
+    vector<int> result = majorityElement(nums);
+    cout << "The final list is: ";
+    for(int x : result) {
+        cout << x << " ";
+    }
+    cout << endl;
+    return 0;
+}
